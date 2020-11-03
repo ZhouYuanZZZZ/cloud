6dd466660020233ad79f7bef258b8932d82c54d0
ZhouYuanZZZZ <zy1270157082@outlook.com>
2020-11-04 00:49:43 +0800

    Initial commit

---
+++ cloud-provider-payment8001/src/main/java/org/example/springcloud/entities/CommonResult.java
@@ -0,0 +1,14 @@
+package org.example.springcloud.entities;
+
+public class CommonResult<T> {
+
+    private Integer code;
+    private String message;
+    private T data;
+
+    public CommonResult(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+}
